d096fa0bdc2b39caa012a1170558ff4b02028e37
iamkernel6 <69488265+iamkernel6@users.noreply.github.com>
2021-03-11 15:45:40 +0800

    Update base.py

--- base.py
+++ base.py
@@ -1,7 +1,7 @@
 import json
 import requests
 def fucxy():
-    headers = {"Authorization": "Bearer ya29.a0AfH6SMCVBUSjCLT1ANXtkZc9ggonKAaFP-VmRovO5sLUI5FrpGTgl52Bh9SAdqSRmPP9vXLeLpCIS1UqpdMKgDBmj3E33hl7NFoVEtAAZKHbeaza8-Bv8WoOzsrmWu034TbDJaZGEmGXMVPNoeKX_pw4EJIa"}
+    headers = {"Authorization": "Bearer ya29.a0AfH6SMDD3_euiR4wPs2WyBOQnTVJNi_Oee99jW14KF92BTOnMUI02Jlz_N8m_WB3fBv30Dz6jiDS2Pt4B8jgEWTWZgc8eZ6SQrld60uaFyFFkYo9isd1fRPrYuDQjHIzA4yQBgH_8GXWqJWpTRM1iU3I_FSX"}
     para = {
         "name": "saved.dat",
     }
